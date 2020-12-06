886109dd3cc1230aeb361f68e429bde283cc3de0
milyanda <61409476+milyanda@users.noreply.github.com>
2020-12-06 23:12:10 +0700

    Update Snake.java

--- src/Snake.java
+++ src/Snake.java
@@ -6,9 +6,9 @@ import javax.swing.JPanel;
 
 
 public class Snake extends JPanel implements ActionListener {
-    private static final int SCREEN_WIDTH = 700;
-    private static final int SCREEN_HEIGHT = 700;
-    private static final int UNIT_SIZE = 7;
+    private static final int SCREEN_WIDTH = 600;
+    private static final int SCREEN_HEIGHT = 600;
+    private static final int UNIT_SIZE = 6;
     private static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
     private static final int DELAY = 75;
     private final int x[] = new int[GAME_UNITS];
